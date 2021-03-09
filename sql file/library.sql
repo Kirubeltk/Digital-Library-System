dc13eaaacabc7292c18dbbcd081e0449dd93ed26
Kirubeltk <52244043+Kirubeltk@users.noreply.github.com>
2021-03-09 10:19:24 +0300

    Update library.sql

--- sql file/library.sql
+++ sql file/library.sql
@@ -1,22 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
+-- version 4.1.6
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2018 at 05:27 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Mar 09, 2021 at 08:18 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `library`
@@ -28,22 +26,51 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `FullName` varchar(100) DEFAULT NULL,
   `AdminEmail` varchar(120) DEFAULT NULL,
   `UserName` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `admin`
 --
---
 
 INSERT INTO `admin` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `updationDate`) VALUES
-(1, 'Anuj Kumar', 'phpgurukulofficial@gmail.com', 'admin', 'f925916e2754e5e03f75dd58a5733251', '2017-07-16 18:11:42');
+(12, 'Kirubel', 'kirubel@gmail.com', 'kirubel', 'e10adc3949ba59abbe56e057f20f883e', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `o`
+--
+
+CREATE TABLE IF NOT EXISTS `o` (
+  `y` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `i` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `o`
+--
+
+INSERT INTO `o` (`y`, `i`) VALUES
+(NULL, 10),
+('2021-02-10 11:13:31', 454);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t`
+--
+
+CREATE TABLE IF NOT EXISTS `t` (
+  `y` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -51,24 +78,24 @@ INSERT INTO `admin` (`id`, `FullName`, `AdminEmail`, `UserName`, `Password`, `up
 -- Table structure for table `tblauthors`
 --
 
-CREATE TABLE `tblauthors` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tblauthors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `AuthorName` varchar(159) DEFAULT NULL,
   `creationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `tblauthors`
 --
 
 INSERT INTO `tblauthors` (`id`, `AuthorName`, `creationDate`, `UpdationDate`) VALUES
-(1, 'Anuj kumar', '2017-07-08 12:49:09', '2017-07-08 15:16:59'),
-(2, 'Chetan Bhagatt', '2017-07-08 14:30:23', '2017-07-08 15:15:09'),
-(3, 'Anita Desai', '2017-07-08 14:35:08', NULL),
-(4, 'HC Verma', '2017-07-08 14:35:21', NULL),
-(5, 'R.D. Sharma ', '2017-07-08 14:35:36', NULL),
-(9, 'fwdfrwer', '2017-07-08 15:22:03', NULL);
+(1, 'Kirubel', '2021-02-10 12:26:50', NULL),
+(11, 'Donald Knuth', '2021-03-09 05:34:46', NULL),
+(12, 'Robert Sedgewick', '2021-03-09 05:35:06', NULL),
+(13, 'Steve McConnell', '2021-03-09 05:35:22', NULL),
+(14, 'Brian Kernighan', '2021-03-09 05:35:42', NULL);
 
 -- --------------------------------------------------------
 
@@ -76,24 +103,33 @@ INSERT INTO `tblauthors` (`id`, `AuthorName`, `creationDate`, `UpdationDate`) VA
 -- Table structure for table `tblbooks`
 --
 
-CREATE TABLE `tblbooks` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tblbooks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `BookName` varchar(255) DEFAULT NULL,
   `CatId` int(11) DEFAULT NULL,
   `AuthorId` int(11) DEFAULT NULL,
   `ISBNNumber` int(11) DEFAULT NULL,
   `BookPrice` int(11) DEFAULT NULL,
   `RegDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `tblbooks`
 --
 
 INSERT INTO `tblbooks` (`id`, `BookName`, `CatId`, `AuthorId`, `ISBNNumber`, `BookPrice`, `RegDate`, `UpdationDate`) VALUES
-(1, 'PHP And MySql programming', 5, 1, 222333, 20, '2017-07-08 20:04:55', '2017-07-15 05:54:41'),
-(3, 'physics', 6, 4, 1111, 15, '2017-07-08 20:17:31', '2017-07-15 06:13:17');
+(6, 'gold', 6, 10, 12345, NULL, '2021-02-10 12:28:47', '2021-02-12 03:53:00'),
+(7, 'cvb', 4, 3, 0, NULL, '2021-02-12 03:57:08', NULL),
+(8, 'dfgdfg', 4, 2, 2325, NULL, '2021-02-12 04:00:12', NULL),
+(9, 'dfdf', 4, 2, 214345, NULL, '2021-02-12 04:01:47', NULL),
+(10, 'Barron''s AP Computer Science A, 8th Edition', 2, 12, 1235, NULL, '2021-03-09 05:36:33', NULL),
+(11, 'Introduction to Artificial Intelligence', 2, 14, 789, NULL, '2021-03-09 05:38:52', NULL),
+(12, 'History of the World Map', 3, 11, 741, NULL, '2021-03-09 05:39:58', NULL),
+(13, 'Land of seven rivers: History of India''s Geography', 3, 12, 852, NULL, '2021-03-09 05:40:40', NULL),
+(14, 'Principles of mathematical analysis', 1, 14, 963, NULL, '2021-03-09 05:42:18', NULL),
+(15, 'Meditations on First Philosophy', 12, 11, 458, NULL, '2021-03-09 05:43:08', NULL);
 
 -- --------------------------------------------------------
 
@@ -101,60 +137,34 @@ INSERT INTO `tblbooks` (`id`, `BookName`, `CatId`, `AuthorId`, `ISBNNumber`, `Bo
 -- Table structure for table `tblcategory`
 --
 
-CREATE TABLE `tblcategory` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tblcategory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `CategoryName` varchar(150) DEFAULT NULL,
   `Status` int(1) DEFAULT NULL,
   `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `UpdationDate` timestamp NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `tblcategory`
 --
 
 INSERT INTO `tblcategory` (`id`, `CategoryName`, `Status`, `CreationDate`, `UpdationDate`) VALUES
-(4, 'Romantic', 1, '2017-07-04 18:35:25', '2017-07-06 16:00:42'),
-(5, 'Technology', 1, '2017-07-04 18:35:39', '2017-07-08 17:13:03'),
-(6, 'Science', 1, '2017-07-04 18:35:55', '0000-00-00 00:00:00'),
-(7, 'Management', 0, '2017-07-04 18:36:16', '0000-00-00 00:00:00');
+(1, 'Maths', 1, '2021-03-09 05:29:04', '2021-03-09 05:31:17'),
+(2, 'Computer Science', 1, '2021-03-09 05:29:57', '2021-03-09 05:31:33'),
+(3, 'Geography', 1, '2021-03-09 05:30:23', '2021-03-09 05:31:41'),
+(11, 'Management', 1, '2021-03-09 05:31:55', '0000-00-00 00:00:00'),
+(12, 'Philosophy', 1, '2021-03-09 05:33:26', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblissuedbookdetails`
+-- Table structure for table `tblguest`
 --
 
-CREATE TABLE `tblissuedbookdetails` (
-  `id` int(11) NOT NULL,
-  `BookId` int(11) DEFAULT NULL,
-  `StudentID` varchar(150) DEFAULT NULL,
-  `IssuesDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `ReturnDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `RetrunStatus` int(1) DEFAULT NULL,
-  `fine` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblissuedbookdetails`
---
-
-INSERT INTO `tblissuedbookdetails` (`id`, `BookId`, `StudentID`, `IssuesDate`, `ReturnDate`, `RetrunStatus`, `fine`) VALUES
-(1, 1, 'SID002', '2017-07-15 06:09:47', '2017-07-15 11:15:20', 1, 0),
-(2, 1, 'SID002', '2017-07-15 06:12:27', '2017-07-15 11:15:23', 1, 5),
-(3, 3, 'SID002', '2017-07-15 06:13:40', NULL, 0, NULL),
-(4, 3, 'SID002', '2017-07-15 06:23:23', '2017-07-15 11:22:29', 1, 2),
-(5, 1, 'SID009', '2017-07-15 10:59:26', NULL, 0, NULL),
-(6, 3, 'SID011', '2017-07-15 18:02:55', NULL, 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblstudents`
---
-
-CREATE TABLE `tblstudents` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tblguest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `StudentId` varchar(100) DEFAULT NULL,
   `FullName` varchar(120) DEFAULT NULL,
   `EmailId` varchar(120) DEFAULT NULL,
@@ -162,101 +172,105 @@ CREATE TABLE `tblstudents` (
   `Password` varchar(120) DEFAULT NULL,
   `Status` int(1) DEFAULT NULL,
   `RegDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `StudentId` (`StudentId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `tblguest`
+--
+
+INSERT INTO `tblguest` (`id`, `StudentId`, `FullName`, `EmailId`, `MobileNumber`, `Password`, `Status`, `RegDate`, `UpdationDate`) VALUES
+(11, 'SID012', 'Kirubel', 'sdgsd@gmail.com', '21646', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-02-09 07:07:06', NULL),
+(12, 'SID013', 'Adane', 'adnae@gmail.com', '464646', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-02-09 15:56:55', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblissuedbookdetails`
+--
+
+CREATE TABLE IF NOT EXISTS `tblissuedbookdetails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `BookId` int(11) DEFAULT NULL,
+  `StudentID` varchar(150) DEFAULT NULL,
+  `IssuesDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `ReturnDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `RetrunStatus` int(1) DEFAULT NULL,
+  `fine` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+
+--
+-- Dumping data for table `tblissuedbookdetails`
+--
+
+INSERT INTO `tblissuedbookdetails` (`id`, `BookId`, `StudentID`, `IssuesDate`, `ReturnDate`, `RetrunStatus`, `fine`) VALUES
+(9, 3, 'SID016', '2021-02-10 12:29:17', '2021-02-10 12:36:49', 1, NULL),
+(10, 11, 'SID013', '2021-03-09 06:03:28', NULL, NULL, NULL),
+(11, 12, 'SID020', '2021-03-09 06:03:59', '2021-03-09 06:07:56', 1, NULL),
+(12, 15, 'SID023', '2021-03-09 06:04:34', NULL, NULL, NULL),
+(13, 12, 'SID024', '2021-03-09 06:05:17', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbllibrarian`
+--
+
+CREATE TABLE IF NOT EXISTS `tbllibrarian` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `FullName` varchar(100) DEFAULT NULL,
+  `LibrarianEmail` varchar(120) DEFAULT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data for table `tbllibrarian`
+--
+
+INSERT INTO `tbllibrarian` (`id`, `FullName`, `LibrarianEmail`, `UserName`, `Password`, `updationDate`) VALUES
+(14, 'Yared', 'yared@gmail.com', 'yaredk', 'e10adc3949ba59abbe56e057f20f883e', '0000-00-00 00:00:00'),
+(16, 'Aweke Amanuel', 'awekea@gmail.com', 'awekea', 'e10adc3949ba59abbe56e057f20f883e', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudents`
+--
+
+CREATE TABLE IF NOT EXISTS `tblstudents` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `StudentId` varchar(100) DEFAULT NULL,
+  `FullName` varchar(120) DEFAULT NULL,
+  `EmailId` varchar(120) DEFAULT NULL,
+  `MobileNumber` char(11) DEFAULT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `Status` int(1) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `StudentId` (`StudentId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
 -- Dumping data for table `tblstudents`
 --
 
 INSERT INTO `tblstudents` (`id`, `StudentId`, `FullName`, `EmailId`, `MobileNumber`, `Password`, `Status`, `RegDate`, `UpdationDate`) VALUES
-(1, 'SID002', 'Anuj kumar', 'anuj.lpu1@gmail.com', '9865472555', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-11 15:37:05', '2017-07-15 18:26:21'),
-(4, 'SID005', 'sdfsd', 'csfsd@dfsfks.com', '8569710025', '92228410fc8b872914e023160cf4ae8f', 0, '2017-07-11 15:41:27', '2017-07-15 17:43:03'),
-(8, 'SID009', 'test', 'test@gmail.com', '2359874527', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-11 15:58:28', '2017-07-15 13:42:44'),
-(9, 'SID010', 'Amit', 'amit@gmail.com', '8585856224', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-15 13:40:30', NULL),
-(10, 'SID011', 'Sarita Pandey', 'sarita@gmail.com', '4672423754', 'f925916e2754e5e03f75dd58a5733251', 1, '2017-07-15 18:00:59', NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblauthors`
---
-ALTER TABLE `tblauthors`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblbooks`
---
-ALTER TABLE `tblbooks`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblcategory`
---
-ALTER TABLE `tblcategory`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblissuedbookdetails`
---
-ALTER TABLE `tblissuedbookdetails`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tblstudents`
---
-ALTER TABLE `tblstudents`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `StudentId` (`StudentId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `tblauthors`
---
-ALTER TABLE `tblauthors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `tblbooks`
---
-ALTER TABLE `tblbooks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `tblcategory`
---
-ALTER TABLE `tblcategory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `tblissuedbookdetails`
---
-ALTER TABLE `tblissuedbookdetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `tblstudents`
---
-ALTER TABLE `tblstudents`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
+(12, 'SID013', 'Adane Mola', 'adnae@gmail.com', '0912348577', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-02-09 15:56:55', '2021-03-09 06:20:38'),
+(19, 'SID020', 'Jhon Abreha', 'john@gmail.com', '+2519415879', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-02-10 12:42:05', NULL),
+(20, 'SID021', 'Yilma Kassa', 'yilmak@yahoo.com', '0911468254', 'fcea920f7412b5da7be0cf42b8c93759', 1, '2021-03-09 05:56:05', NULL),
+(21, 'SID022', 'Moges Haile', 'moges.haile@gmail.com', '0911987458', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-03-09 05:56:54', NULL),
+(22, 'SID023', 'Rediet Yilma', 'rediety@gmail.com', '0911154245', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-03-09 05:57:37', NULL),
+(23, 'SID024', 'Berhanu Belete', 'berhanubelete@hotmail.com', '091345683', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-03-09 05:58:31', NULL),
+(24, 'SID025', 'Hiwot Alemu', 'hiwota@gmail.com', '0913548796', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-03-09 05:59:01', NULL),
+(25, 'SID026', 'Tsion Abebe', 'tsiona@gmail.com', '0911452356', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-03-09 06:00:00', NULL),
+(26, 'SID027', 'Misgan Tareke', 'misgan@gmail.com', '0911365487', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-03-09 06:01:36', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
